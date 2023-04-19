7a097d9f6005f648596dcb913eea66f080c1ef84
haonhat0703 <haonhat2729@gmail.com>
2023-04-19 21:31:41 +0700

    helper file

---
+++ utils.py
@@ -0,0 +1,5 @@
+from PIL import ImageFont
+
+char_list = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+font = ImageFont.truetype("DejaVuSansMono-Bold.ttf", size=15)
+FOLDER_RESULT = "Image_result1"
